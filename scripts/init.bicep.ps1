5d598d8ef8e3cf682c417118f8fdd8036647258c
Alexander Schmidt <alexander.schmidt@devdeer.com>
2025-05-24 11:34:15 +0200

    changed bicep init

--- scripts/init.bicep.ps1
+++ scripts/init.bicep.ps1
@@ -3,28 +3,54 @@
 #
 # Copyright DEVDEER GmbH 2024
 # Latest update: 2023-03-25
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [switch]
+    $PreRelease
+)
 
 if ($PSScriptRoot.Contains(' ') -and $PSScriptRoot -ne $PWD) {
     throw "This script needs to be executed from inside its folder because white spaces where detected."
 }
 $root = $PSScriptRoot.Contains(' ') ? '.' : $PSScriptRoot
 
-nuget install devdeer.Templates.Bicep -Source nuget.org -Prerelease -OutputDirectory $root
-
-# remove existing modules and components
-if (Test-Path -Path "$root/modules") {
-    Remove-Item "$root/modules" -Recurse
+$package = 'devdeer.templates.bicep'
+$versionUrl = "https://api.nuget.org/v3-flatcontainer/$($package.ToLower())/index.json"
+$result = Invoke-WebRequest -Uri $versionUrl
+$json = $result.Content | ConvertFrom-Json
+$pos = -1;
+while ($true) {
+    $version = $json.versions[$pos]
+    if ($PreRelease.IsPresent) {
+        break
+    }
+    if ($version.Contains('-') -eq $false) {
+        break
+    }
+    $pos--
 }
-if (Test-Path -Path "$root/components") {
-    Remove-Item "$root/components" -Recurse
+$packageNuget = "$($package.ToLower()).$($version.ToLower()).nupkg"
+$downloadUrl = "https://api.nuget.org/v3-flatcontainer/$($package.ToLower())/$($version.ToLower())/$packageNuget"
+Invoke-WebRequest -Uri $downloadUrl -OutFile $packageNuget
+Expand-Archive $packageNuget tmp
+Remove-Item $packageNuget
+$folders = @( 'components', 'constants', 'functions', 'modules', 'types' )
+if (!(Test-Path -Path "bicepSettings.json")) {
+    Move-Item "$root/tmp/assets/bicepSettings.json" $root -Force
 }
-# move items modules and components one level up from the nuget path
-Move-Item "$root/devdeer.Templates.Bicep*/modules" $root -Force
-Move-Item "$root/devdeer.Templates.Bicep*/components" $root -Force
-# try to remove the nuget installation package
-Remove-Item "$root/devdeer.Templates.Bicep*" -Recurse -Force -ErrorAction SilentlyContinue
+if (!(Test-Path -Path ".gitignore")) {
+    Move-Item "$root/tmp/assets/.gitignore" $root -Force
+}
+foreach($folder in $folders) {
+    if (Test-Path $folder) {
+        Remove-Item -Force -Recurse $folder
+    }
+    Move-Item -Force "$root/tmp/$folder" $root
+}
+Remove-Item -Force -Recurse $root/tmp
+Write-Host "Using version $version of devdeer.Template.Bicep now"
 
 # download the scripts and content from GitHub
-curl https://raw.githubusercontent.com/DEVDEER/spock-content/main/scripts/deploy.bicep.ps1 -OutputDirectory $root
-curl https://raw.githubusercontent.com/DEVDEER/spock-content/main/scripts/build.bicep.ps1 -OutputDirectory $root
-curl https://raw.githubusercontent.com/DEVDEER/spock-content/main/static/bicepContext.json -OutputDirectory $root
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DEVDEER/spock-content/main/scripts/deploy.bicep.ps1" -OutFile "$root/deploy.bicep.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DEVDEER/spock-content/main/scripts/build.bicep.ps1" -OutFile "$root/build.bicep.ps1"
