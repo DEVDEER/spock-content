1e0ede74066bdd8be100dbd2c0fd2d897daa4c01
Alexander Schmidt <32797407+devdeer-alex@users.noreply.github.com>
2025-11-11 19:49:02 +0100

    Modify Swashbuckle CLI installation to list tools
    Updated script to display installed tools after installation.

--- scripts/update-apim-api.ps1
+++ scripts/update-apim-api.ps1
@@ -98,8 +98,9 @@ function Install-DotnetTool() {
         Write-Host "Done"
     }
     Write-Host "Ensuring Swashbuckle CLI..." -NoNewline
-    dotnet tool install Swashbuckle.AspNetCore.Cli --version 6.7.3 | Out-Null
+    dotnet tool install Swashbuckle.AspNetCore.Cli --version 6.7.3
     Write-Host "Done"
+    dotnet tool list
 }
 
 function Get-ProjectFileName() {
