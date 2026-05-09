3192a9dcbe42c6569f6fdfc67228393fcaadc304
Alexander Schmidt <alexander.schmidt@devdeer.com>
2026-05-09 16:54:45 +0200

    Adding paths

--- scripts/Redeploy-Aci.ps1
+++ scripts/Redeploy-Aci.ps1
@@ -24,12 +24,13 @@ Write-Host "Version: 1.0"
 # Get credentials
 $clientId = (az ad sp list --display-name $DeploySpName --query "[0].appId" -o tsv).Trim()
 $password = (az keyvault secret show --vault-name $DeploySpKeyVaultName -n $DeploySpKeyVaultKey --query value -o tsv).Trim()
+Write-Host "Client id for $DeploySpName is $clientId."
 # Extract what you need from existing config
 $newImage = "$($ContainerImageName):$ContainerImageTagToDeploy"
 # Redeploy using az container create with existing config exported as YAML
-az container export --resource-group $ResourceGroup --name $AciName --file aci-config.yaml
+az container export --resource-group $ResourceGroup --name $AciName --file ./aci-config.yaml
 # Patch the YAML
-$content = Get-Content aci-config.yaml
+$content = Get-Content ./aci-config.yaml
 # Override the container image tag
 $regex = "image: $($ContainerImageName):\S+"
 $content = $content -replace $regex, "image: $newImage"
@@ -51,11 +52,11 @@ if ($LogAnalyticsKey.Length -gt 0) {
     }
 }
 # Overwrite the file
-$content | Set-Content aci-config.yaml
+$content | Set-Content ./aci-config.yaml
 $content
 # Redeploy from YAML — credentials need to be injected separately as they're not exported
 az container create --resource-group $ResourceGroup `
-    --file aci-config.yaml `
+    --file ./aci-config.yaml `
     --registry-login-server $AcrName `
     --registry-username $clientId `
     --registry-password $password `
