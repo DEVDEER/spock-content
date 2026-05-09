cc746669bf5e83599c1ed758c93de9e2a8388670
Alexander Schmidt <alexander.schmidt@devdeer.com>
2026-05-09 16:26:20 +0200

    Fixes for ACI deploy script

--- scripts/Redeploy-Aci.ps1
+++ scripts/Redeploy-Aci.ps1
@@ -20,37 +20,36 @@ param (
     $LogAnalyticsKey = ''
 )
 $ErrorActionPreference = 'Stop'
-# Get current container group config
-$aci = (az container show --resource-group $ResourceGroup --name $AciName -o json) | ConvertFrom-Json
 # Get credentials
 $clientId = (az ad sp list --display-name $DeploySpName --query "[0].appId" -o tsv).Trim()
 $password = (az keyvault secret show --vault-name $DeploySpKeyVaultName -n $DeploySpKeyVaultKey --query value -o tsv).Trim()
 # Extract what you need from existing config
-$container = $aci.containers[0]
 $newImage = "$($ContainerImageName):$ContainerImageTagToDeploy"
 # Redeploy using az container create with existing config exported as YAML
 az container export --resource-group $ResourceGroup --name $AciName --file aci-config.yaml
 # Patch the YAML
-# 1. Override the container image tag
-$searchTag = $ContainerImageName #.Replace('.', '\.')
 $content = Get-Content aci-config.yaml
-$regex = "image: $($searchTag):\S+"
+# Override the container image tag
+$regex = "image: $($ContainerImageName):\S+"
 $content = $content -replace $regex, "image: $newImage"
-# 2. Remove unsupported settings
+# Override API version
+$regex = "apiVersion:\S+"
+$content = $content -replace $regex, "2025-04-01"
+# Remove unsupported settings
 $content = $content -replace "provisioningTimeoutInSeconds: \S+", ""
-# 4. Add log analytics key after workspaceId
+# Add log analytics key after workspaceId
 if ($LogAnalyticsKey.Length -gt 0) {
     # there is a workspace id configured
     $regex = '^(\s*)workspaceId:\s*.+'
-    $content = foreach ($line in $content) {        
+    $content = foreach ($line in $content) {
         $line
-        if ($line -match $regex) {            
+        if ($line -match $regex) {
             $indent = $matches[1]
-            "${indent}workspaceKey: $LogAnalyticsKey"            
+            "${indent}workspaceKey: $LogAnalyticsKey"
         }
     }
 }
-# 3. Overwrite the file
+# Overwrite the file
 $content | Set-Content aci-config.yaml
 # Redeploy from YAML — credentials need to be injected separately as they're not exported
 az container create --resource-group $ResourceGroup `
